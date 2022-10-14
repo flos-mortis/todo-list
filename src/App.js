e3a73d866ed04451cd206cc9bf6b9d16ad45d4a6
Sylu <mortisflos@gmail.com>
2022-10-14 20:31:05 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import todosData from './todosData';
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id)
+        {
+          
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+  render(){
+    const todoItems = this.state.todos.map((item) => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+
+    return (
+      <div className='todo-list'>
+        <Header/>
+          {todoItems}
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
